906b81d7df020a76514f02b0d1e3e1983294cd7c
kwangkim <kkim@qcc.cuny.edu>
2015-02-03 07:28:49 -0500

    Update updatetab.js

--- js/updatetab.js
+++ js/updatetab.js
@@ -1,23 +1,5 @@
 function updatetab(url, pane) {
 // http://docs.mathjax.org/en/latest/tex.html
-MathJax.Hub.Config({
-  tex2jax: {
-    inlineMath: [['$','$'], ['\\(','\\)']],
-    processEscapes: true
-  }
-});
-// http://kerzol.github.io/markdown-mathjax/editor.html
-marked.setOptions({
-  renderer: new marked.Renderer(),
-  gfm: true,
-  tables: true,
-  breaks: false,
-  pedantic: false,
-  sanitize: false, // IMPORTANT, because we do MathJax before markdown,
-                   //            however we do escaping in 'CreatePreview'.
-  smartLists: true,
-  smartypants: false
-});
     // From http://stackoverflow.com/a/651735
     var ext = url.split('.').pop().toLowerCase();
     var ismd = true;

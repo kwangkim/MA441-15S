7df58e5ed231f0fb1bfcc0d66c37d190f15f5092
kwangkim <kkim@qcc.cuny.edu>
2015-02-03 22:07:33 -0500

    Update updatetab.js

--- js/updatetab.js
+++ js/updatetab.js
@@ -8,7 +8,7 @@ function updatetab(url, pane) {
     }
     $.get(url, function(data) {
         if (ismd) {
-          console.log("replace!");
+          console.log("re!");
           data= data.replace('_','\_');   
           // Handle underscore by \_
           $('#my-pagination-content').html(marked(data));

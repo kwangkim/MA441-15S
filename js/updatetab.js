303b3762f0538b79db3a3e1950b389446f31a879
kwangkim <kkim@qcc.cuny.edu>
2015-02-04 19:17:30 -0500

    Update updatetab.js

--- js/updatetab.js
+++ js/updatetab.js
@@ -13,7 +13,7 @@ var Latexdown={
     this.previewid=pid;
     this.eventtabid=eid;
     this.preview=document.getElementById(this.previewid);
-    var initial=$(document.getElementById(this.eventtabid)).find(".active a"); 
+    var initial=$("#event-tab a .active"); 
     console.log(initial);
     console.log(initial.attr("data-url"));
     this.UpdateTab(initial.attr("data-url"),initial);

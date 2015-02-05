9509480d2d17df94e0d1bfd70cce994a36c86a13
kwangkim <kkim@qcc.cuny.edu>
2015-02-04 20:04:42 -0500

    Update updatetab.js

--- js/updatetab.js
+++ js/updatetab.js
@@ -1,31 +1,46 @@
 var Latexdown={
-  delay: 150,        // delay after keystroke before updating
+  delay: 50,        // delay after keystroke before updating
 
   previewid: null,
-  eventtapid: null,
+  bufferbid: null,
+  eventtabid: null,
   preview: null,     // filled in by Init below
+  buffer: null,  
 
   timeout: null,     // store setTimout id
   mjRunning: false,  // true when MathJax is processing
   oldText: null,     // used to check if an update is needed
   
-  Init: function (pid,eid) {
+  Init: function (pid,bid,eid) {
     this.previewid=pid;
+    this.bufferid=bid;
     this.eventtabid=eid;
     this.preview=document.getElementById(this.previewid);
-  }, 
+    this.buffer=document.getElementById(this.bufferid);
+  },
+  SwapBuffers: function () {
+    var buffer = this.preview, preview = this.buffer;
+    this.buffer = buffer; this.preview = preview;
+    buffer.style.display = "none";
+    buffer.style.position = "absolute";
+    preview.style.position = ""; 
+    preview.style.display = "";
+  },
   Update: function () {
     if (this.timeout) {clearTimeout(this.timeout)}
     this.timeout = setTimeout(this.callback,this.delay);
   },
   PreviewHtml: function () {
     this.mjRunning = false;
+    this.SwapBuffers();
   },
   PreviewMark: function () {
     this.mjRunning = false;
-    text = this.preview.innerHTML;
+    text = this.buffer.innerHTML;
     text = text.replace(/^&gt;/mg, '>');
-    this.preview.innerHTML = marked (text);
+    this.buffer.innerHTML = marked (text);
+    this.SwapBuffers();
+    
   },
   IsMarkdown: function(url){
     ext = url.split('.').pop().toLowerCase();  
@@ -43,18 +58,26 @@ var Latexdown={
         if (_this.mjRunning) return;
         var text = data;
         if (text === _this.oldtext) return;
-        _this.preview.innerHTML = _this.oldtext = text;
+        text = this.Escape(text);
+        _this.buffer.innerHTML = _this.oldtext = text;
         _this.mjRunning = true;
-        console.log(url);
         if(_this.IsMarkdown(url)  ){
-            MathJax.Hub.Queue(["Typeset",MathJax.Hub,_this.preview],["PreviewMark",_this]);
+            MathJax.Hub.Queue(["Typeset",MathJax.Hub,_this.buffer],["PreviewMark",_this]);
         }
         else{
-            MathJax.Hub.Queue(["Typeset",MathJax.Hub,_this.preview],["PreviewHTML",_this]);
+            MathJax.Hub.Queue(["Typeset",MathJax.Hub,_this.buffer],["PreviewHTML",_this]);
         }
         makerbutton();
         pane.tab('show');
     });
+  },
+  Escape: function (html, encode) {
+    return html
+      .replace(!encode ? /&(?!#?\w+;)/g : /&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+     .replace(/'/g, '&#39;');
   }
 };
 
